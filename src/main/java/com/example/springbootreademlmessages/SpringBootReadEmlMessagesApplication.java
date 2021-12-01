2f23554d0d1c5a86665e94945af11e8d22f27b5d
muharremkoc <kmuho5885@gmail.com>
2021-12-01 17:18:10 +0300

    Spring-Boot-mail-info-with-POP3

---
+++ src/main/java/com/example/springbootreademlmessages/SpringBootReadEmlMessagesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootreademlmessages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReadEmlMessagesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReadEmlMessagesApplication.class, args);
+    }
+
+}
